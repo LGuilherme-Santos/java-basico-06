0c0627fb191f09ab55ea10ccc6ac42083156c230
Guilherme <guisrsk8@gmail.com>
2020-03-07 23:48:27 -0300

    segundo commit

--- src/br/fai/projeto06/Main.java
+++ src/br/fai/projeto06/Main.java
@@ -1,5 +1,7 @@
 package br.fai.projeto06;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -11,12 +13,42 @@ public class Main {
 
 	private void start() {
 		System.out.println("Exercicio 06");
+		System.out.println("");
 		
-		String[] a = {"civic", "gol", "palio", "uno"};
+		Scanner scan = new Scanner(System.in);
+		String[] a = new String[4];
 		
 		for(int i = 0; i < a.length; i++){
-			System.out.println(a[i]);
+			System.out.println("Nome do carro: ");
+		a[i] = scan.next();
 		}
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println("");
+		
+		for(int i = 0; i < a.length; i++){
+			System.out.println("Nome do carro: ");
+		a[i] = scan.next();
+		}
+		
+		for(String carro: a){
+						
+			System.out.print(carro + " ");
+		}
+		System.out.println(" ");
+		
+		for(int i = 0; i < a.length; i++){
+			System.out.println("Nome do carro: ");
+		a[i] = scan.next();
+		}
+		
+		for(String carro: a){
+						
+			System.out.print(carro + " ");
+		}
+		
 		
 	}
 }
